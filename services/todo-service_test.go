57543cb6f5191fd1acf7ee9891aa18550ed7d5ea
WhiteTiger0369 <baoit98@gmail.com>
2021-12-27 18:53:13 +0700

    edit ut

--- services/todo-service_test.go
+++ services/todo-service_test.go
@@ -18,10 +18,13 @@ func TestTodoService_FindAll(t *testing.T) {
 			Title:   "abc",
 			Content: "abc",
 		},
-	}, common.DatabaseError{Type: "abc", Code: 1})
+	}, common.DatabaseError{Type: "", Code: 0})
 
 	todoService := NewTodoService(mockTodoRepo)
-	res, _ := todoService.FindAll()
+	res, err := todoService.FindAll()
+	if err.Code != 0 {
+		t.Errorf("Todo not matching")
+	}
 	if len(res) != 1 {
 		t.Errorf("Todo not matching")
 	}
@@ -31,10 +34,13 @@ func TestTodoService_FindByID(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	mockTodoRepo := mocks.NewMockTodoRepository(mockCtrl)
-	mockTodoRepo.EXPECT().FindByID(gomock.Any()).Return(entities.Todo{Title: "abc", Content: "abc"}, common.DatabaseError{Type: "abc", Code: 1})
+	mockTodoRepo.EXPECT().FindByID(gomock.Any()).Return(entities.Todo{Title: "abc", Content: "abc"}, common.DatabaseError{Type: "", Code: 0})
 
 	todoService := NewTodoService(mockTodoRepo)
-	res, _ := todoService.FindByID(1)
+	res, err := todoService.FindByID(1)
+	if err.Code != 0 {
+		t.Errorf("Todo not matching")
+	}
 	if res.Title != "abc" {
 		t.Errorf("Todo not matching")
 	}
@@ -45,10 +51,13 @@ func TestTodoService_Save(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	mockTodoRepo := mocks.NewMockTodoRepository(mockCtrl)
-	mockTodoRepo.EXPECT().Save(gomock.Any()).Return(entities.Todo{Title: "abc", Content: "abc"}, common.DatabaseError{Type: "abc", Code: 1})
+	mockTodoRepo.EXPECT().Save(gomock.Any()).Return(entities.Todo{Title: "abc", Content: "abc"}, common.DatabaseError{Type: "", Code: 0})
 
 	todoService := NewTodoService(mockTodoRepo)
-	res, _ := todoService.Save(todo)
+	res, err := todoService.Save(todo)
+	if err.Code != 0 {
+		t.Errorf("Todo not matching")
+	}
 	if res.Title != "abc" {
 		t.Errorf("Todo not matching")
 	}
@@ -58,11 +67,41 @@ func TestTodoService_FindByUserId(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	mockTodoRepo := mocks.NewMockTodoRepository(mockCtrl)
-	mockTodoRepo.EXPECT().FindByUserId(gomock.Any()).Return([]entities.Todo{{Title: "abc", Content: "abc"}}, common.DatabaseError{Type: "abc", Code: 1})
+	mockTodoRepo.EXPECT().FindByUserId(gomock.Any()).Return([]entities.Todo{{Title: "abc", Content: "abc"}}, common.DatabaseError{Type: "", Code: 0})
 
 	todoService := NewTodoService(mockTodoRepo)
-	res, _ := todoService.FindByUserId(1)
+	res, err := todoService.FindByUserId(1)
+	if err.Code != 0 {
+		t.Errorf("Todo not matching")
+	}
 	if len(res) != 1 {
+		t.Errorf("Todo not matching")
+	}
+}
+
+func TestTodoService_Delete(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockTodoRepo := mocks.NewMockTodoRepository(mockCtrl)
+	mockTodoRepo.EXPECT().Delete(gomock.Any())
+
+	todoService := NewTodoService(mockTodoRepo)
+	todoService.Delete(uint(1))
+}
+
+func TestTodoService_Update(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockTodoRepo := mocks.NewMockTodoRepository(mockCtrl)
+	mockTodoRepo.EXPECT().FindByID(gomock.Any()).Return(entities.Todo{Title: "abc", Content: "abc"}, common.DatabaseError{Type: "", Code: 0})
+	mockTodoRepo.EXPECT().Save(gomock.Any()).Return(entities.Todo{Title: "abc", Content: "abc"}, common.DatabaseError{Type: "", Code: 0})
+
+	todoService := NewTodoService(mockTodoRepo)
+	res, err := todoService.Update(uint(1), dtos.TodoDTO{})
+	if err.Code != 0 {
+		t.Errorf("Todo not matching")
+	}
+	if res.Title != "abc" {
 		t.Errorf("Todo not matching")
 	}
 }
